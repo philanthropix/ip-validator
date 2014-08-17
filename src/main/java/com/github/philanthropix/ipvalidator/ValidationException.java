a033cb4d4ef64cef9363389d53cb73af31765541
philanthropix <philanthropix99@gmail.com>
2014-08-17 22:40:17 +0100

    removed as unneeded

--- src/main/java/com/github/philanthropix/ipvalidator/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.github.philanthropix.ipvalidator;
-
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
